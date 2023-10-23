3b97df778549caccef20fbe524f1105271f853b8
lhbl <lhbllxily@outlook.com>
2023-10-23 22:17:58 +0800

    Springboot_tlias_dome

---
+++ src/main/java/com/dreamstu/springtlias/SpringtliasApplication.java
@@ -0,0 +1,13 @@
+package com.dreamstu.springtlias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtliasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtliasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dreamstu/springtlias/SpringtliasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamstu.springtlias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtliasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
